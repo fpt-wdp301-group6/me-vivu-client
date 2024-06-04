e38d996d8d0d2219372e287dad7811798ea4a455
Syaoran <jinnguyen054@gmail.com>
2024-06-04 17:09:40 +0700

    Read More component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as MovieCard } from './movie-card';
 export { default as MovieCarousel } from './movie-carousel';
 export { default as MovieImage } from './movie-image';
+export { default as ReadMore } from './read-more';
 export { default as TrailerModal } from './trailer-modal';
